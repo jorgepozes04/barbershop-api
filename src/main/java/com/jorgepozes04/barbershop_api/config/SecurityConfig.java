85abc0ab69a1067eccf0e7aa1f0ff99fecbc2463
Jorge Pozes <jorgepozes027@gmail.com>
2026-03-07 14:54:43 -0300

    config: update swagger version

--- src/main/java/com/jorgepozes04/barbershop_api/config/SecurityConfig.java
+++ src/main/java/com/jorgepozes04/barbershop_api/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/appointments/available-slots").permitAll()
 
                         .requestMatchers(HttpMethod.POST, "/barbers").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/clients").hasAnyAuthority("ROLE_ADMIN", "ROLE_BARBER")
                         .requestMatchers("/clients/me/**").hasAuthority("ROLE_CLIENT")
 
                         .anyRequest().authenticated())
